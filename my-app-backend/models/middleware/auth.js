3bdd1653c7ae24834698997745745ea0c42d24fa
haseeb_cubix <haseeb.khan@cubixlabs.com>
2022-08-19 12:17:37 +0500

    changes

--- my-app-backend/models/middleware/auth.js
+++ my-app-backend/models/middleware/auth.js
@@ -1,23 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const authorization = req.headers.authorization
-    if (!authorization) {
-        return res.status(404).json({ message: "Not authorized!" })
-    }
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(404).json({ message: "Not authorized!" });
+  }
 
-    const token = authorization.split(" ")[1]
+  const token = authorization.split(" ")[1];
 
-    if (!token) {
-        return res.status(404).json({ message: "Not authorized!" })
-    }
-
-    try {
-        const decode = jwt.verify(token, 'secret123');
-        console.log(decode)
-        req.userData = decode;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: error.message });
-    }
-}
+  if (!token) {
+    return res.status(404).json({ message: "Not authorized!" });
+  }
+  try {
+    const decode = jwt.verify(token, "secret123");
+    console.log(decode);
+    req.userData = decode;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
